753fa0b4ae0a88d9e30e844659a2c75f716368dd
TerekhovaIS <75006405+TerekhovaIS@users.noreply.github.com>
2020-12-03 15:26:32 +0300

    Update test_items.py
    добавление рабочего кода в файл

--- test_items.py
+++ test_items.py
@@ -1,10 +1,13 @@
 import time
 
-
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_page_contains_add_to_cart_button(browser):
-    browser.get(link)
-    browser.find_element_by_class_name("btn-add-to-basket")
-    # time.sleep(30) if you need
-    assert True, "Add to cart button not found"
+def test_guest_should_see_login_link_pass(browser):
+    try:
+        browser.get(link)
+        basket_button = browser.find_element_by_css_selector(".btn-add-to-basket")
+        result = True
+        # time.sleep(30) if you need
+    except:
+        result = False
+        assert result == False, "Element not found"
